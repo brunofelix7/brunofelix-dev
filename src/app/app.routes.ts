33fe94a3eb5b4e3e4245054fd257277b925ada64
Bruno Felix <brunofelix.dev@gmail.com>
2026-08-08 23:21:35 -0300

    🔧 Adding '/home', '/certificates' and '/cv' routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./core/components/shell/shell').then((m) => m.Shell),
+    children: [
+      {
+        path: 'home',
+        loadComponent: () => import('./features/home/home').then((m) => m.Home),
+      },
+      {
+        path: 'certificates',
+        loadComponent: () =>
+          import('./features/certificates/certificates').then((m) => m.Certificates),
+      },
+      {
+        path: 'cv',
+        loadComponent: () => import('./features/cv/cv').then((m) => m.Cv),
+      },
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home',
+      },
+    ],
+  },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavBar } from './core/components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavBar],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
