172645bcc1b0bfb210689f7e3cdc63da18fff624
Bruno Felix <brunofelix.dev@gmail.com>
2026-08-09 23:20:50 -0300

    ✨ Creating 'container' component for responsive layout structure

---
+++ src/app/shared/components/container/container.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-container',
+  templateUrl: './container.html',
+  styleUrls: ['./container.scss'],
+  standalone: true,
+})
+export class ContainerComponent {}
