f4e832cba9945da283797aa4d3ca2577420ffa9c
Bruno Felix <brunofelix.dev@gmail.com>
2026-09-20 22:30:09 -0300

    ✨ Replacing the PosTrainer project with Movies Explorer and supporting detail images

--- src/app/shared/models/project.model.ts
+++ src/app/shared/models/project.model.ts
@@ -3,6 +3,7 @@ export interface Project {
     title: string;
     description: string;
     image: string;
+    detailImage?: string;
     tags: string[];
     youtubeUrl?: string;
     sourceUrl?: string;
